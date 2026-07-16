588c05bf19bb77c6480c2a69611188b2f719e7bf
ASAN-INJU <jjukssam755@gmail.com>
2026-07-17 02:50:00 +0900

    Use environment variable for server port

--- api-server.js
+++ api-server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //--------------------------------------
 // 환경변수
